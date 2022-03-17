5370aec0a2fdd1f7f9c0571c21137e8a8ddc76e6
DavSnek <da.sniezek@gmail.com>
2022-03-17 10:29:41 +0100

    Changed window size.

--- EMSolver2D/MainForm.h
+++ EMSolver2D/MainForm.h
@@ -48,11 +48,17 @@ namespace EMSolver2D {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MainForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1100, 600);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
